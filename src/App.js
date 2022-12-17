eb830ceeef526d3a356aa558768e87f85279cab2
Edwin Cervantes <ecer1996@gmail.com>
2022-12-17 01:54:39 -0700

    Añadiendo Titulo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Nav from './componentes/Nav';
+import Titulo from './componentes/Titulo';
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <Nav/>
+       <Titulo/>
       </header>
     </div>
   );
---
+++ src/componentes/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Titulo() {
+  return (
+    <div className='contenedor-titulo'>
+        <h1>What Items to Store?</h1>
+        <p>Select which items you want to store before moving to<br/>your new home.We´ll keep´em safe</p>
+    </div>
+  )
+}
+
+export default Titulo
